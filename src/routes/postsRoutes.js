ec655fe7ff49a4ee6c60bf74e6daadc39b70acfe
Caiorossi00 <caio_rossi56@hotmail.com>
2024-11-22 11:11:10 -0300

    aula 5 - backend completo

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -4,7 +4,14 @@ import {
   listarPosts,
   postarNovoPost,
   uploadImagem,
+  atualizarNovoPost,
 } from "../controllers/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,12 +26,14 @@ const upload = multer({ storage: storage });
 
 const routes = (app) => {
   app.use(express.json());
-
+  app.use(cors(corsOptions));
   app.get("/posts", listarPosts);
 
   app.post("/posts", postarNovoPost);
 
   app.post("/upload", upload.single("imagem"), uploadImagem);
+
+  app.put("/upload/:id", atualizarNovoPost);
 };
 
 export default routes;
